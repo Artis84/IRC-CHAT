a1292dfa20c49af08b14a4ef3317e830386daec4
Artis84 <45531582+Artis84@users.noreply.github.com>
2022-11-18 17:28:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require("express");
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message info codeco', function(msgInfoCoDeco){
-  io.emit('chat message info codeco','A user connected');
-  });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-    socket.on('chat message info codeco', function(msgInfoCoDeco){
-    io.emit('chat message info codeco','A user disconnected');
+io.on("connection", function (socket) {
+    console.log("a user connected");
+    socket.on("chat message info codeco", function (msgInfoCoDeco) {
+        io.emit("chat message info codeco", "A user connected");
     });
-  });  
+    socket.on("disconnect", function () {
+        console.log("user disconnected");
+        socket.on("chat message info codeco", function (msgInfoCoDeco) {
+            io.emit("chat message info codeco", "A user disconnected");
+        });
+    });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-  	console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
+io.on("connection", function (socket) {
+    socket.on("chat message", function (msg) {
+        console.log("message: " + msg);
+        io.emit("chat message", msg);
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log("listening on *:3000");
 });
